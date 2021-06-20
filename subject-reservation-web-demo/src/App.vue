397f9facd4feb8d82df30d01ea783be673c9e31a
wirawatroj <wirawat.roj@gmail.com>
2021-06-21 02:02:01 +0700

    [Add] init project

---
+++ subject-reservation-web-demo/src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <v-app>
+    <v-app-bar
+      app
+      color="blue"
+      dark
+    >
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Name"
+          class="shrink mt-1 hidden-sm-and-down"
+          contain
+          min-width="100"
+          src="https://www.sit.kmutt.ac.th/wp-content/uploads/2018/05/logo-flat-blk.png"
+          width="120"
+        />
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+        text
+      >
+        <span class="mr-2">Latest Release</span>
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <v-container>
+        <router-view/>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ subject-reservation-web-demo/src/views/Home.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="home">
+    <br>
+    <p style="font-family:Roboto; font-size:18pt">SIT Subject Reservation Web</p>
+    <br>
+    <v-row justify="center" align="top" no-gutters style="height: 150px;">
+      <v-col v-for="n in 3" :key="n" cols="3">
+        <subject/>
+      </v-col>
+    </v-row>
+  </div>
+</template>
+
+
+<script>
+  import Subject from '../components/Subject'
+  export default {
+    name: 'Home',
+    components: {
+      Subject,
+    },
+    data: () => ({
+      alignments: [
+        'start',
+        'center',
+        'end',
+      ],
+    })
+  }
+</script>
