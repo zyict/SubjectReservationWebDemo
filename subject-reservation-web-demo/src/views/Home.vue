cbff31d8cea871be7799483e2c5d3b1aa184944e
zyct <thanyaluck.yict@mail.kmutt.ac.th>
2021-08-07 14:11:41 +0700

    [update] netlify title

--- subject-reservation-web-demo/src/views/Home.vue
+++ subject-reservation-web-demo/src/views/Home.vue
@@ -2,7 +2,7 @@
   <div class="home">
     <br />
     <p style="font-family: Roboto; font-size: 18pt">
-      SIT Subject Reservation Web on feature/list-subjects
+      SIT Subject Reservation Web netlify
     </p>
     <br />
     <v-row justify="center" align="top" no-gutters style="height: 150px">
